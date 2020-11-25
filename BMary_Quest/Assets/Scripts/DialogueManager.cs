a43685c600034aa8be86b0bb0bb96d47868aa475
Emilie <emivog0223@skola.goteborg.se>
2020-11-25 15:14:16 +0100

    created dialogue
    dialogue- main, trigger, manager.

---
+++ BMary_Quest/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    public Text characterName;
+    public Text characterLine;
+
+    int dialogueLine;
+
+    bool started;
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        if (!started)
+        {
+
+            characterName.text = dialogue.name;
+            dialogueLine = 0;
+            started = true;
+        }
+        if (dialogue.sentences.Length > dialogueLine)
+        {
+            characterLine.text = dialogue.sentences[dialogueLine];
+        }
+        else
+        {
+            started = false;
+
+            characterName.text = "";
+            characterLine.text = "";
+        }
+
+       /* Debug.Log("Starting conversation with " + dialogue.name);*/
+
+
+
+        dialogueLine++;
+    }
+
+
+}
