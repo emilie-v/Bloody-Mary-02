7a5adcf1d7633f97a0914b562f241f4968dce4d6
TosaLipkiss <tovahl2002@skola.goteborg.se>
2020-11-25 15:27:32 +0100

    IngameGUI_Canvas, blood points added and placeholder graphic
    Added placeholders GUI, bloodpoint bar function added.

---
+++ BMary_Quest/Assets/Scripts/BloodPointsBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BloodPointsBar : MonoBehaviour
+{
+    public Slider bloodSlider;
+
+    public void maxBlood(int blood)
+    {
+        bloodSlider.maxValue = blood;
+        bloodSlider.value = blood;
+    }
+
+    public void SetBlood(int blood)
+    {
+        bloodSlider.value = blood;
+    }
+}
---
+++ BMary_Quest/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxBlood = 20;
+    public int currentBlood;
+
+    private void Start()
+    {
+        currentBlood = maxBlood;
+    }
+
+    private void Update()
+    {
+        //if den andra checkar ut sina brickor, förlorar denna spelare blood points
+    }
+
+    void loseBlood(int blood)
+    {
+        currentBlood -= blood;
+    }
+}
