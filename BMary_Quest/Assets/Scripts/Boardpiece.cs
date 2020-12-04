2804284ff03e19b7d2ae3fd786c977cc2eb2607a
TosaLipkiss <tovahl2002@skola.goteborg.se>
2020-12-04 09:57:55 +0100

    added starter marks

--- BMary_Quest/Assets/Scripts/Boardpiece.cs
+++ BMary_Quest/Assets/Scripts/Boardpiece.cs
@@ -9,27 +9,40 @@ public class Boardpiece : MonoBehaviour
     public Sprite enemys;
     public Sprite nothing;
 
+    Sprite enemyStarter;
+    Sprite maryStarter;
+
     private SpriteRenderer tile;
 
     private GameControl gameControl;
+
     // Start is called before the first frame update
     void Start()
     {
-    piece = GetComponent<SpriteRenderer>();
-    mary = Resources.Load<Sprite>("Sprites/Mark_BloodyMary");
-    enemys = Resources.Load<Sprite>("Sprites/Mark_Lucifer");  
+        piece = GetComponent<SpriteRenderer>();
+        mary = Resources.Load<Sprite>("Sprites/Mark_BloodyMary");
+        enemys = Resources.Load<Sprite>("Sprites/Mark_Lucifer");
+
+        enemyStarter = Resources.Load<Sprite>("Sprites/Marks/Mark_Lucifer_Start");
+        maryStarter = Resources.Load<Sprite>("Sprites/Marks/Mark_BloodyMary_Start");
     }
 
     // Update is called once per frame
     public void Update()
     {
-        if(GetComponentInParent<Owner>().owned == (int)Tile_State.empty) 
-       piece.sprite=nothing;
+        if (GetComponentInParent<Owner>().owned == (int)Tile_State.empty)
+            piece.sprite = nothing;
 
-       if(GetComponentInParent<Owner>().owned == (int)Tile_State.player1) 
-       piece.sprite=mary;
+        if (GetComponentInParent<Owner>().owned == (int)Tile_State.player1)
+            piece.sprite = mary;
 
-       if(GetComponentInParent<Owner>().owned == (int)Tile_State.player2)
-       piece.sprite=enemys;
+        if (GetComponentInParent<Owner>().owned == (int)Tile_State.player2)
+            piece.sprite = enemys;
+
+        if (GetComponentInParent<Owner>().specialState == 1)
+            piece.sprite = maryStarter;
+
+        if (GetComponentInParent<Owner>().specialState == 2)
+            piece.sprite = enemyStarter;
     }
 }
