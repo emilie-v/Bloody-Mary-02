8e52739f92f6e1e71951e3b3c960e78d04e2a138
TosaLipkiss <tovahl2002@skola.goteborg.se>
2020-12-04 13:24:54 +0100

    padlock added on unlocked staffs and opponents

--- BMary_Quest/Assets/Scripts/ChooseStaff.cs
+++ BMary_Quest/Assets/Scripts/ChooseStaff.cs
@@ -9,6 +9,7 @@ public class ChooseStaff : MonoBehaviour
     Sprite[] staffList;
     Sprite mirror;
     Sprite hell;
+    Sprite padlock;
 
     public GameObject chooseEnemy;
     public GameObject chooseStaff;
@@ -16,6 +17,7 @@ public class ChooseStaff : MonoBehaviour
     public GameObject selectWarning;
     public GameObject cancelWarning;
 
+    public GameObject padlockImage;
 
     int index = 0;
     Image currentStaff;
@@ -26,6 +28,7 @@ public class ChooseStaff : MonoBehaviour
 
         mirror = Resources.Load<Sprite>("Sprites/Staffs/Staff_Mirror");
         hell = Resources.Load<Sprite>("Sprites/Staffs/Staff_Hell");
+        padlock = Resources.Load<Sprite>("Sprites/GUI/GUI_padlock");
 
         staffList = new Sprite[2];
         staffList[0] = mirror;
@@ -79,6 +82,18 @@ public class ChooseStaff : MonoBehaviour
     {
         chooseStaff.SetActive(false);
         chooseEnemy.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (index == 0)
+        {
+            padlockImage.SetActive(false);
+        }
+        else if(index == 1)
+        {
+            padlockImage.SetActive(true);
+        }
     }
 
 }
