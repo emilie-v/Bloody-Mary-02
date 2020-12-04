a54d9c153b090c7ae8b43957dbd733ee20bfadde
Callep3 <42645914+Callep3@users.noreply.github.com>
2020-12-04 15:53:18 +0100

    Added TODO
    Quick and easy todo for next weeks AI implementation

---
+++ BMary_Quest/Assets/Scripts/AIBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBehaviour : MonoBehaviour
+{
+    //TODO If AIMode is enabled
+    
+    //TODO First, place Bricks at random
+    //TODO Second, use staff
+    //TODO Third, decide if cashout or not (randomize a thresh hold)
+    //TODO Fourth, end turn
+        
+    //TODO Randomize delay between moves
+}
