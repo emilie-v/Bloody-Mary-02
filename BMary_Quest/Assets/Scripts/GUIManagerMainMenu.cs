cd54afbe39810ecbda607219d67480c25ba4d6f2
Callep3 <42645914+Callep3@users.noreply.github.com>
2020-12-09 15:21:55 +0100

    Added Options Screen
    Added options screen and music volume control

---
+++ BMary_Quest/Assets/Scripts/GUIManagerMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManagerMainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject optionsPanel;
+    
+    public void OptionsPanel()
+    {
+        if (optionsPanel.activeSelf)
+        {
+            optionsPanel.SetActive(false);
+        }
+        else if (!optionsPanel.activeSelf)
+        {
+            optionsPanel.SetActive(true);
+        }
+    }
+}
