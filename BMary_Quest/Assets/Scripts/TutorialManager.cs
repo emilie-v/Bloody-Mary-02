416cf9819329b68d007ac4fede6a95e308cd1647
Emilie <emivog0223@skola.goteborg.se>
2020-12-10 13:03:47 +0100

    update

--- BMary_Quest/Assets/Scripts/TutorialManager.cs
+++ BMary_Quest/Assets/Scripts/TutorialManager.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoBehaviour
 {
+
+    public void BackToMainMenuButton()
+    {
+        SoundManager.Instance.LockedWarningPopUpSound();
+        SceneManager.LoadScene("MainMenu");
+    }
+
 
     void Update()
     {
