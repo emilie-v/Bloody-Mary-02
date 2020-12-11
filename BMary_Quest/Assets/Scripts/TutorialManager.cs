0f70a9508c740cfd3a142de6b3a9378b9057e639
Emilie <emivog0223@skola.goteborg.se>
2020-12-11 11:06:13 +0100

    Added buttons and audio to buttons

--- BMary_Quest/Assets/Scripts/TutorialManager.cs
+++ BMary_Quest/Assets/Scripts/TutorialManager.cs
@@ -5,18 +5,73 @@ using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoBehaviour
 {
+    public GameObject gameBoardTutorial;
+    public GameObject bloodPointsTutorial;
+    public GameObject marksTutorial;
+    public GameObject attackMovesTutorial;
+    public GameObject staffsTutorial;
+    public GameObject endTurnTutorial;
+    public GameObject backTutorial;
 
     public void BackToMainMenuButton()
     {
+        SetAllToFalse();
         SoundManager.Instance.LockedWarningPopUpSound();
         SceneManager.LoadScene("MainMenu");
-
     }
 
-
-    void Update()
+    public void GameBoardTutorialButton()
     {
-       
+        SoundManager.Instance.MenuButtonSound();
+        SetAllToFalse();
+        gameBoardTutorial.SetActive(true);
     }
+
+    public void BloodPointsTutorialButton()
+    {
+        SoundManager.Instance.MenuButtonSound();
+        SetAllToFalse();
+        bloodPointsTutorial.SetActive(true);
+    }
+
+    public void MarksTutorialButton()
+    {
+        SoundManager.Instance.MenuButtonSound();
+        SetAllToFalse();
+        marksTutorial.SetActive(true);
+    }
+
+    public void AttackMovesTutorialButton()
+    {
+        SoundManager.Instance.MenuButtonSound();
+        SetAllToFalse();
+        attackMovesTutorial.SetActive(true);
+    }
+
+    public void StaffsTutorialButton()
+    {
+        SoundManager.Instance.MenuButtonSound();
+        SetAllToFalse();
+        staffsTutorial.SetActive(true);
+    }    
+
+    public void EndTurnTutorialButton()
+    {
+        SoundManager.Instance.MenuButtonSound();
+        SetAllToFalse();
+        endTurnTutorial.SetActive(true);
+    }
+
+    private void SetAllToFalse()
+    {
+        gameBoardTutorial.SetActive(false);
+        bloodPointsTutorial.SetActive(false);
+        marksTutorial.SetActive(false);
+        attackMovesTutorial.SetActive(false);
+        staffsTutorial.SetActive(false);
+        endTurnTutorial.SetActive(false);
+    }
+
+
 
 }
