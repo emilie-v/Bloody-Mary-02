fed172862cd05adf94c80b8de3772c0fca0c365e
TosaLipkiss <tovahl2002@skola.goteborg.se>
2020-12-16 13:48:33 +0100

    PoseAnimation script started wip

---
+++ BMary_Quest/Assets/Scripts/PoseAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseAnimation : MonoBehaviour
+{
+    Sprite[] maryPoses;
+    Sprite maryAngry;
+    Sprite maryDamaged;
+    Sprite maryScared;
+
+    public void Start()
+    {
+        maryAngry = Resources.Load<Sprite>("Sprites/Characters/Mary_Reactions/Pose_Mary_Angry");
+        maryDamaged = Resources.Load<Sprite>("Sprites/Characters/Mary_Reactions/Pose_Mary_Damaged");
+        maryScared = Resources.Load<Sprite>("Sprites/Characters/Mary_Reactions/Pose_Mary_Scared");
+
+        maryPoses = new Sprite[3];
+        maryPoses[0] = maryAngry;
+        maryPoses[1] = maryDamaged;
+        maryPoses[2] = maryScared;
+    }
+}
