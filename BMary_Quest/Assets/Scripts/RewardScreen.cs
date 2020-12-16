2d02ae4e2a357f230cebfcec013ae6a049d6cff2
TosaLipkiss <tovahl2002@skola.goteborg.se>
2020-12-16 11:39:14 +0100

    reward screen implemented. music problem

---
+++ BMary_Quest/Assets/Scripts/RewardScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RewardScreen : MonoBehaviour
+{
+    Sprite[] staffRewardList;
+
+    Sprite pumpkinReward;
+    Sprite countReward;
+    Sprite hellReward;
+
+    public Image currentStaffReward;
+
+    public GameObject rewardScreen;
+
+    void Start()
+    {
+        pumpkinReward = Resources.Load<Sprite>("Sprites/Staffs/Staff_Pumpkin");
+        countReward = Resources.Load<Sprite>("Sprites/Characters/Enemies/Enemy_Count");
+        hellReward = Resources.Load<Sprite>("Sprites/Staffs/Staff_Hell_Portrait");
+
+        staffRewardList = new Sprite[3];
+        staffRewardList[0] = pumpkinReward;
+        staffRewardList[1] = countReward;
+        staffRewardList[2] = hellReward;
+    }
+
+    public void newReward(int index)
+    {
+        currentStaffReward.sprite = staffRewardList[index];
+        rewardScreen.SetActive(true);
+        
+    }
+}
