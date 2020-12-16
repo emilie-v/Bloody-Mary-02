4e4400ca8332c73c4412501c413f1c20beab2ed7
TosaLipkiss <tovahl2002@skola.goteborg.se>
2020-12-16 15:37:31 +0100

    Marys Poses implemented

--- BMary_Quest/Assets/Scripts/PoseAnimation.cs
+++ BMary_Quest/Assets/Scripts/PoseAnimation.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class PoseAnimation : MonoBehaviour
 {
     public Sprite[] maryPoseList;
+    public Sprite maryIdle;
     public Sprite maryAngry;
     public Sprite maryDamaged;
     public Sprite maryScared;
@@ -16,14 +17,18 @@ public class PoseAnimation : MonoBehaviour
 
     public void Start()
     {
+        maryIdle = Resources.Load<Sprite>("Sprites/Characters/Mary_Reactions/Pose_Mary_Idle");
         maryAngry = Resources.Load<Sprite>("Sprites/Characters/Mary_Reactions/Pose_Mary_Angry");
         maryDamaged = Resources.Load<Sprite>("Sprites/Characters/Mary_Reactions/Pose_Mary_Damaged");
         maryScared = Resources.Load<Sprite>("Sprites/Characters/Mary_Reactions/Pose_Mary_Scared");
 
-        maryPoseList = new Sprite[3];
-        maryPoseList[0] = maryAngry;
-        maryPoseList[1] = maryDamaged;
-        maryPoseList[2] = maryScared;
+        maryPoseList = new Sprite[4];
+        maryPoseList[0] = maryIdle;
+        maryPoseList[1] = maryAngry;
+        maryPoseList[2] = maryDamaged;
+        maryPoseList[3] = maryScared;
+
+        updateMaryPose(0);
     }
 
     public void updateMaryPose(int index)
