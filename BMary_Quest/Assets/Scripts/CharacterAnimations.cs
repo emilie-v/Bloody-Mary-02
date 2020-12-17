90388244e3593267623738edc0cbcd1294fbb1e4
TosaLipkiss <tovahl2002@skola.goteborg.se>
2020-12-17 14:00:50 +0100

    Blink animation Idle

---
+++ BMary_Quest/Assets/Scripts/CharacterAnimations.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimations : MonoBehaviour
+{
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            animator.SetTrigger("Idle");
+        }
+    }
+}
