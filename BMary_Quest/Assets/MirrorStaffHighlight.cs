e11694a7ad7c792d2efd72cd773879f8b24a42e9
Callep3 <42645914+Callep3@users.noreply.github.com>
2020-12-21 09:32:08 +0100

    Added script

---
+++ BMary_Quest/Assets/MirrorStaffHighlight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MirrorStaffHighlight : MonoBehaviour,
+    IPointerEnterHandler,
+    IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+}
