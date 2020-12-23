81dac5d71984a55cf1544f9aec2cb24d32487fc3
Mellowtide-CH <stavlama@hotmail.com>
2020-12-23 12:23:30 +0100

    Update DialogueTrigger.cs

--- BMary_Quest/Assets/Scripts/DialogueTrigger.cs
+++ BMary_Quest/Assets/Scripts/DialogueTrigger.cs
@@ -13,11 +13,11 @@ public class DialogueTrigger : MonoBehaviour
     //greetings
     private string[][] EnemyGreetings =
     {
-        new string[] {"And now we finally meet, Bloody Mary…!","Sorry, just came out of the brimstone showers.","Have you been a bad girl this year?","There’s no need for this, dear pet. We can rule the world together!","What’s all this I hear about some witch wanting to steal my staff?"},
-        new string[] {"Oh, Hi there Mary","Don’t go too hard…","Can we talk this out like adults?","Here we go again…","Want to take a drink after this, Mary?"},
-        new string[] {"Hola, Amiga!","Saludos, Mirror lady!","I’ll show you flower power, chica!","Feel spooked yet?","Want a flower, girl?"},
-        new string[] {"*Pant pant*","*Head tilt*","*Bark bark!*"},
-        new string[] {"Well well well… Bloody Mary.","We meet at last, Necromancer.","This won’t take long, my dear.","You got a very pretty nec...name.","A toast to you, my lady!" }
+        new string[] {"And now we finally meet, Bloody Mary…!","Sorry, just came out of the brimstone showers.","Have you been a bad girl this year?","There’s no need for this, dear pet. We can rule the world together!","What’s all this I hear about some witch wanting to steal my staff?",""},
+        new string[] {"Oh, Hi there Mary","Don’t go too hard…","Can we talk this out like adults?","Here we go again…","Want to take a drink after this, Mary?",""},
+        new string[] {"Hola, Amiga!","Saludos, Mirror lady!","I’ll show you flower power, chica!","Feel spooked yet?","Want a flower, girl?",""},
+        new string[] {"*Pant pant*","*Head tilt*","*Bark bark!*",""},
+        new string[] {"Well well well… Bloody Mary.","We meet at last, Necromancer.","This won’t take long, my dear.","You got a very pretty nec...name.","A toast to you, my lady!","" }
     };
 
 
@@ -60,7 +60,7 @@ public class DialogueTrigger : MonoBehaviour
         Debug.Log("We get this far");
         int i = DataAcrossScenes.EnemyChosenStaff;
         Debug.Log("int i= " + i);
-        int j = Random.Range(0, EnemyGreetings[i].GetUpperBound(0));
+        int j = Random.Range(0, EnemyGreetings[i].GetUpperBound(0)); //glöm inte att lägga till en tom string då random range är icke-inklusiv på upper range!
         Debug.Log("int i= "+i + "int j= " +j);
         EnemyText.text = EnemyGreetings[i][j];
     }
