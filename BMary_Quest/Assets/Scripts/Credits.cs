4c8f53d70c2833908ef3286cf5582ad2264b2e75
Callep3 <42645914+Callep3@users.noreply.github.com>
2021-01-05 13:55:36 +0100

    Added hotkey back function and fixed order in ingamegui

--- BMary_Quest/Assets/Scripts/Credits.cs
+++ BMary_Quest/Assets/Scripts/Credits.cs
@@ -1,13 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
+    private string _scene;
+    
     public void Menu()
     {
         SoundManager.Instance.LockedWarningPopUpSound();
         SceneManager.LoadScene("MainMenu");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && _scene == "Credits")
+        {
+            Menu();
+        }
+    }
+    
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        _scene = scene.name;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
